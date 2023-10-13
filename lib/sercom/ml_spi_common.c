b1d66c3191daa6427a02b702b82f41bc5b2af756
Mason Lopez <masonlopez@me.com>
2023-10-13 12:02:20 -0400

    adding comments to spi_common

--- lib/sercom/ml_spi_common.c
+++ lib/sercom/ml_spi_common.c
@@ -1,49 +1,69 @@
 /*
  * Author: Ben Westcott, Jayson De La Vega
  * Date created: 8/11/23
+ *
+ * Editted: Mason Lopez 10/13/2023
  */
 
 #include <ml_spi_common.h>
 
+/**
+ * enables SPI communication for the given Sercom pad,
+ * hangs until the system acknowledges the change.
+ */
 void spi_enable(Sercom *coms)
 {
     coms->SPI.CTRLA.bit.ENABLE = 0x01;
-    while(coms->SPI.SYNCBUSY.bit.ENABLE != 0U)
+    while (coms->SPI.SYNCBUSY.bit.ENABLE != 0U)
     {
         /* Wait for sync */
     }
 }
 
+/**
+ * diables SPI communication for the gicen Sercom pad,
+ * hangs until the system acknowledges the change.
+ */
 void spi_disable(Sercom *coms)
 {
     coms->SPI.CTRLA.bit.ENABLE = 0x00;
-    while(coms->SPI.SYNCBUSY.bit.ENABLE != 0U)
+    while (coms->SPI.SYNCBUSY.bit.ENABLE != 0U)
     {
         /* Wait for sync */
     }
 }
 
+/**
+ * Software reset on the sercom pad.
+ */
 void spi_swrst(Sercom *coms)
 {
     coms->SPI.CTRLA.bit.SWRST = 0x01;
-    while(coms->SPI.SYNCBUSY.bit.SWRST != 0U)
+    while (coms->SPI.SYNCBUSY.bit.SWRST != 0U)
     {
         /* Wait for sync */
     }
 }
 
+/**
+ * Enables recieving data for SPI on sercom pad
+ */
 void spi_reciever_enable(Sercom *coms)
 {
     coms->SPI.CTRLB.bit.RXEN = true;
-    while(coms->SPI.SYNCBUSY.bit.CTRLB != 0U)
+    while (coms->SPI.SYNCBUSY.bit.CTRLB != 0U)
     {
         /* Wait for sync */
     }
 }
+
+/**
+ * Disables recieving data for SPI on sercom pad.
+ */
 void spi_reciever_disable(Sercom *coms)
 {
     coms->SPI.CTRLB.bit.RXEN = false;
-    while(coms->SPI.SYNCBUSY.bit.CTRLB != 0U)
+    while (coms->SPI.SYNCBUSY.bit.CTRLB != 0U)
     {
         /* Wait for sync */
     }
@@ -51,12 +71,12 @@ void spi_reciever_disable(Sercom *coms)
 
 uint32_t spi_read32b(Sercom *coms, uint32_t data)
 {
-    while(coms->SPI.INTFLAG.bit.DRE == 0)
+    while (coms->SPI.INTFLAG.bit.DRE == 0)
     {
         /* Wait for data ready flag */
     }
     coms->SPI.DATA.reg = data;
-    while(coms->SPI.INTFLAG.bit.RXC == 0)
+    while (coms->SPI.INTFLAG.bit.RXC == 0)
     {
         /* Wait for recieve complete intflag */
     }
@@ -65,89 +85,84 @@ uint32_t spi_read32b(Sercom *coms, uint32_t data)
 
 uint8_t spi_read8b(Sercom *coms, uint8_t data)
 {
-    while(coms->SPI.INTFLAG.bit.DRE == 0)
+    while (coms->SPI.INTFLAG.bit.DRE == 0)
     {
         /* Wait for data ready flag */
     }
     coms->SPI.DATA.reg = data;
-    while(coms->SPI.INTFLAG.bit.RXC == 0)
+    while (coms->SPI.INTFLAG.bit.RXC == 0)
     {
         /* Wait for recieve complete intflag */
     }
     return (uint8_t)coms->SPI.DATA.reg;
 }
 
-void spi_dmac_tx_init
-(
+/**
+ * Setups DMAC for transmitting
+*/
+void spi_dmac_tx_init(
     ml_dmac_s *dmac_s,
     Sercom *coms_inst,
-    DmacDescriptor *cpy
-)
+    DmacDescriptor *cpy)
 {
+    // get channel number for transfer channel
     ml_dmac_chnum_t chnum = dmac_s->ex_chnum;
 
-    DMAC_channel_init
-    (
+    // init DMAC
+    DMAC_channel_init(
         chnum,
         dmac_s->chan_settings,
-        dmac_s->chan_prilvl
-    );
+        dmac_s->chan_prilvl);
 
     uint32_t btsize = DMAC_extract_btsize(dmac_s->descriptor_settings);
 
-    DMAC_descriptor_init
-    (
+    // setup of descriptor 
+    DMAC_descriptor_init(
         dmac_s->descriptor_settings,
         dmac_s->ex_len,
         (uint32_t)dmac_s->ex_ptr + (btsize * dmac_s->ex_len),
         (uint32_t)&coms_inst->SPI.DATA.reg,
-        (uint32_t) cpy, cpy
-    );
+        (uint32_t)cpy, cpy);
 
-    DMAC_channel_intenset
-    (
+    // setup DMAC interrupt on data transfer and set priority
+    DMAC_channel_intenset(
         chnum,
         dmac_s->irqn,
         dmac_s->intmsk,
-        dmac_s->irqn_prilvl
-    );
-
+        dmac_s->irqn_prilvl);
 }
 
-void spi_dmac_rx_init
-(
+/**
+ * Sets up DMAC for recieving
+*/
+void spi_dmac_rx_init(
     ml_dmac_s *dmac_s,
     Sercom *coms_inst,
-    DmacDescriptor *cpy
-)
+    DmacDescriptor *cpy)
 {
+    // get channel number
     ml_dmac_chnum_t chnum = dmac_s->ex_chnum;
 
-    DMAC_channel_init
-    (
+    // init channel and set priority
+    DMAC_channel_init(
         chnum,
         dmac_s->chan_settings,
-        dmac_s->chan_prilvl
-    );
+        dmac_s->chan_prilvl);
 
     uint32_t btsize = DMAC_extract_btsize(dmac_s->descriptor_settings);
 
-    DMAC_descriptor_init
-    (
+    // set descriptor
+    DMAC_descriptor_init(
         dmac_s->descriptor_settings,
         dmac_s->ex_len,
         (uint32_t)&coms_inst->SPI.DATA.reg,
         (uint32_t)dmac_s->ex_ptr + (btsize * dmac_s->ex_len),
-        (uint32_t) cpy, cpy
-    );
+        (uint32_t)cpy, cpy);
 
-    DMAC_channel_intenset
-    (
+    // setup interrupts for DMAC
+    DMAC_channel_intenset(
         chnum,
         dmac_s->irqn,
         dmac_s->intmsk,
-        dmac_s->irqn_prilvl
-    );
+        dmac_s->irqn_prilvl);
 }
-
-
--- src/spi_slave.cpp
+++ src/spi_slave.cpp
@@ -11,11 +11,14 @@
 #include <stdbool.h>
 #include <SPI.h>
 
+// build flag so we can select which program to upload
 #ifndef BUILD_SERCOM_MASTER
 
 static DmacDescriptor base_descriptor[3] __attribute__((aligned(16)));
 static volatile DmacDescriptor wb_descriptor[3] __attribute__((aligned(16)));
 
+
+// allocated space for RX and TX buffers
 #define SPI_RX_BUFFER_LEN 6
 volatile uint32_t spi_rx_buffer[SPI_RX_BUFFER_LEN];
 
@@ -27,46 +30,56 @@ volatile uint32_t spi_tx_buffer[SPI_TX_BUFFER_LEN] =
     0x00000000, 0x00000000,
 };
 
+
+// create SPI object
 ml_spi_s spi_s = sercom1_spi_dmac_slave_prototype;
 
+// get DMAC channel numbers for rx and tx
 const uint8_t rx_dmac_chnum = spi_s.rx_dmac_s.ex_chnum;
 const uint8_t tx_dmac_chnum = spi_s.tx_dmac_s.ex_chnum;
 
-
 void setup(void)
 {
+    // create serial connection and wait til a device connects
     Serial.begin(9600);
     while(!Serial){
-
+        // this will run indef until you create a serial connection w computer
     }
 
-    Serial.println("Starting slave");
+    Serial.println("Starting servant");
 
+    // starting the clocks
     MCLK_init();
     GCLK_init();
 
+
+    // start the DMAC
     DMAC_init(&base_descriptor[0], &wb_descriptor[0]);
 
+    // enable the SERCOM1 pad for SPI mode 
     sercom1_spi_init(OPMODE_SLAVE);
 
+    // setup DMAC for receiving data, pointing where data should be stored
     spi_s.rx_dmac_s.ex_ptr = &spi_rx_buffer[0];
     spi_s.rx_dmac_s.ex_len = SPI_RX_BUFFER_LEN;
-
     spi_dmac_rx_init(&spi_s.rx_dmac_s, SERCOM1, &base_descriptor[rx_dmac_chnum]);
 
+    // setup DMAC for transmitting data, pointing where data should be sent from
     spi_s.tx_dmac_s.ex_ptr = &spi_tx_buffer[0];
     spi_s.tx_dmac_s.ex_len = SPI_TX_BUFFER_LEN;
-
     spi_dmac_tx_init(&spi_s.tx_dmac_s, SERCOM1, &base_descriptor[tx_dmac_chnum]);
 
+    // enable DMAC and turn respective channels on
     ML_DMAC_ENABLE();
     ML_DMAC_CHANNEL_ENABLE(rx_dmac_chnum);
     ML_DMAC_CHANNEL_ENABLE(tx_dmac_chnum);
 
+    // enable spi on SERCOM1 pad
     spi_enable(SERCOM1);
     spi_reciever_enable(SERCOM1);
 }
 
+// when select pin has been pulled low this means the master wants to communicat
 _Bool ssl_intflag = false;
 void SERCOM1_3_Handler(void)
 {
@@ -74,6 +87,8 @@ void SERCOM1_3_Handler(void)
     ML_SERCOM_SPI_SSL_CLR_INTFLAG(SERCOM1);
 }
 
+// interrupt for reciever DMAC 
+// when transfer is complete this is called
 _Bool dmac_rx_intflag = false;
 void DMAC_0_Handler(void)
 {
@@ -84,6 +99,8 @@ void DMAC_0_Handler(void)
     }
 }
 
+// iterrupt for transmitter DMAC
+// when transfer is complete this is called
 _Bool dmac_tx_intflag = false;
 void DMAC_1_Handler(void)
 {
@@ -93,6 +110,8 @@ void DMAC_1_Handler(void)
         dmac_tx_intflag = true;
     }
 }
+
+
 void loop(void)
 {
     if(ssl_intflag)
@@ -102,6 +121,7 @@ void loop(void)
         Serial.println("ssl pulled");
     }
 
+    // if rx and tx are complete transferring data when read out new data in RX from master
     if(dmac_rx_intflag & dmac_tx_intflag)
     {
         for(uint8_t i=0; i < SPI_RX_BUFFER_LEN; i++)
